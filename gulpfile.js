993d30245023d9bb730f7549336d81d1b5485909
Luke Harvey <luke@lukeharvey.co.uk>
2019-09-15 20:13:26 -0400

    Update dependencies, configure babel, autoprefixer, prettier

--- gulpfile.js
+++ gulpfile.js
@@ -2,82 +2,36 @@
  * Gulpfile.js by Luke Harvey
  */
 
-'use strict';
+"use strict";
+
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")({ lazy: true });
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
 /**
- * Include Gulp & tools we'll use
- */
-
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')({lazy: true});
-
-var runSequence = require('run-sequence');
-var browserSync = require('browser-sync').create();
-
-/**
- * Help
+ * Process Sass
  *
- * List the available gulp tasks
+ * Compile and minify the Sass files and auto-inject into browsers
  */
 
-gulp.task('help', $.taskListing);
-gulp.task('default', ['help']);
-
-/**
- * Build Sass
- *
- * Complie and minify the Sass files and auto-inject into browsers
- */
-
-gulp.task('build-sass', function() {
-  return gulp.src(['./src/sass/main.scss'])
+function sass() {
+  var plugins = [autoprefixer(), cssnano()];
+  return gulp
+    .src("./src/sass/main.scss")
     .pipe($.sourcemaps.init())
-      .pipe($.sass({
+    .pipe(
+      $.sass({
         precision: 10
-      }).on('error', $.sass.logError))
-      .pipe($.autoprefixer({browsers: ['last 2 versions']}))
-      .pipe($.cssnano())
-      .pipe($.rename('main.min.css'))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
-});
-
-/**
- * Build JS
- *
- * Concat and minify the JavaScript files
- */
-
-gulp.task('build-js', function() {
-  return gulp.src(['./src/js/vendor/*.js', './src/js/modules/*.js'])
-    .pipe($.sourcemaps.init())
-      .pipe($.concat('main.min.js'))
-      .pipe($.uglify({preserveComments: 'some'}))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js/'));
-});
-
-/**
- * Clean
- *
- * Clean the dist assets folder
- */
-
-gulp.task('clean', function () {
-  return gulp.src('dist/*', {read: false})
-    .pipe($.clean());
-});
-
-/**
- * Build
- *
- * Build everything
- */
-
-gulp.task('build', function(done) {
-  return runSequence('clean', ['build-sass', 'build-js'], done);
-});
+      }).on("error", $.sass.logError)
+    )
+    .pipe($.postcss(plugins))
+    .pipe($.rename("main.min.css"))
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/css/"))
+    .pipe(browserSync.stream({ match: "**/*.css" }));
+}
 
 /**
  * Lint Sass
@@ -85,36 +39,90 @@ gulp.task('build', function(done) {
  * Lint the Sass files using Stylelint
  */
 
-gulp.task('lint-sass', function lintCssTask() {
-  return gulp.src('src/sass/**/*.scss')
-    .pipe($.stylelint({
-      reporters: [{formatter: 'string', console: true}],
+function lintSass(cb) {
+  gulp.src("./src/sass/**/*.scss").pipe(
+    $.stylelint({
+      reporters: [{ formatter: "string", console: true }],
       syntax: "scss"
-    }));
-});
+    })
+  );
+  cb();
+}
 
 /**
- * Lint JS
+ * Process JS
+ *
+ * Process the JavaScript files
+ */
+
+const js = gulp.parallel(jsModules, jsVendor);
+
+function jsModules() {
+  return gulp
+    .src("./src/js/modules/*.js")
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.concat("modules.min.js"))
+    .pipe($.uglify({ output: { comments: "some" } }))
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/js/"));
+}
+
+function jsVendor() {
+  return gulp
+    .src("./src/js/vendor/*.js")
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.concat("vendor.min.js"))
+    .pipe($.uglify({ output: { comments: "some" } }))
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/js/"));
+}
+
+/**
+ * Lint JS (modules only)
  *
  * Lint the JavaScript files using ESLint
  */
 
-gulp.task('lint-js', function() {
-  return gulp.src(['./src/js/modules/*.js'])
+function lintJS(cb) {
+  gulp
+    .src("./src/js/modules/*.js")
     .pipe($.eslint())
     .pipe($.eslint.format())
-    .pipe($.eslint.failOnError());
-});
+    .pipe($.eslint.failAfterError());
+  cb();
+}
 
 /**
- * Lint
+ * Clean
  *
- * Lint everything
+ * Clean the dist assets folder
  */
 
-gulp.task('lint', function(done) {
-  return runSequence('lint-sass', 'lint-js', done);
-});
+function clean() {
+  return gulp.src("dist/*", { read: false }).pipe($.clean());
+}
+
+/**
+ * Reload
+ */
+
+function reload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+/**
+ * Watch
+ */
+
+function watch() {
+  gulp.watch("./src/sass/**/*.scss", sass);
+  gulp.watch("./src/js/modules/*.js", gulp.series(jsModules, reload));
+  gulp.watch("./src/js/vendor/*.js", gulp.series(jsVendor, reload));
+  gulp.watch("./**/*.html", reload);
+}
 
 /**
  * Serve
@@ -122,17 +130,20 @@ gulp.task('lint', function(done) {
  * Run a Browsersync server and watch all the files.
  */
 
-gulp.task('serve', function() {
-  return runSequence('build', function() {
-    browserSync.init({
-      server: {
-        baseDir: "./"
-      },
-      notify: false
-    });
-
-    gulp.watch(['./src/sass/**/*.scss'], ['build-sass']);
-    gulp.watch(['./src/js/**/*.js'], ['build-js', browserSync.reload]);
-    gulp.watch(['./**/*.html']).on('change', browserSync.reload);
+function serve() {
+  return browserSync.init({
+    injectChanges: true,
+    open: false,
+    server: {
+      baseDir: "./"
+    }
   });
-});
+}
+
+exports.lint = gulp.parallel(lintSass, lintJS);
+exports.build = gulp.series(clean, gulp.parallel(sass, js));
+exports.serve = gulp.series(
+  gulp.parallel(sass, js),
+  gulp.parallel(watch, serve)
+);
+exports.default = serve;
